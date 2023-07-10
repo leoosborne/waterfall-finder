0c7dd3ea17ea61517d5ecf2cd1a214dd00a78aea
Leo Osborne <leo@leocast.com>
2023-07-10 19:52:14 -0400

    created node.js server with express and nodemon

--- src/server.js
+++ src/server.js
@@ -3,6 +3,20 @@ const app = express();
 
 app.use(express.json());
 
+let router = express.Router();
+
+router.get('/users', (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({hello: 'world'}));
+})
+
+router.get('/users/:groups/:id', (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({hello: req.params.groups + 'world' + req.params.id}));
+})
+
+app.use('/api', router);
+
 //middleware to serve static files
 app.use(express.static('../public'));
 
