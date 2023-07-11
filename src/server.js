75f64c5ccb78f285f385fcd8c2b123a84ed64856
Leo Osborne <leo@leocast.com>
2023-07-11 18:51:35 -0400

    updated server.js to send API request to accuweather

--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
 let router = express.Router();
+
+router.get('/recentRain', (req, res, next) => {
+    fetch(`http://dataservice.accuweather.com/currentconditions/v1/333336/historical/24/PrecipitationSummary/Past24Hours/Imperial?apikey=${process.env.ACCUWEATHER_API_KEY}`)  
+    .then(response => {
+        response.json()
+            .then(json => {
+                console.log(json);
+            })
+    })
+
+    .catch(error => {
+        console.error(error)
+    })
+})
 
 router.get('/users', (req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
@@ -24,3 +40,4 @@ app.use(express.static('../public'));
 app.listen(3000, () => {
     console.log('App is listening on port 3000');
 }); 
+ 
